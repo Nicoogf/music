ef17b47689ee19944b1f5fcbb9773db8814cd623
Nicoogf <nicolasgfalabella@gmail.com>
2023-05-31 17:56:32 -0300

    Configuracion de las rutas

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router,Routes,Route  } from 'react-router-dom'
+import Library from './Library'
+import Feed from './Feed'
+import Trending from './Trending'
+import Player from './Player'
+import Favorites from './Favorites'
 
 export default function Home() {
   return (
-    <div> Home </div>
+
+    <Router>
+      <Routes>
+
+        <Route path='/' element={ <Library /> } />
+        <Route path='/feed' element={ <Feed /> } />
+        <Route path='/trending' element={ <Trending /> } />
+        <Route path='/player' element={ <Player /> } />
+        <Route path='/favorites' element={ <Favorites /> } />
+          
+
+      </Routes>  
+    </Router>
+    
   )
 }
 
