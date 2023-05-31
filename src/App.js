621a482e334f2e7cb33d711f8b4975c7cb92c446
Nicoogf <nicolasgfalabella@gmail.com>
2023-05-31 18:51:53 -0300

    Creacion del nav lateral y body principal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Home from './screens/home'
+import Home from './screens/home/home'
 
 export default function App() {
   return (
-    
+
     <div> 
 
       <Home />
--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,25 +1,33 @@
 import React from 'react'
 import { BrowserRouter as Router,Routes,Route  } from 'react-router-dom'
-import Library from './Library'
-import Feed from './Feed'
-import Trending from './Trending'
-import Player from './Player'
-import Favorites from './Favorites'
+
+import Library from '../Library'
+import Feed from '../Feed'
+import Trending from '../Trending'
+import Player from '../Player'
+import Favorites from '../Favorites'
+import './home.css'
+import Sidebar from '../../components/sidebar'
 
 export default function Home() {
   return (
 
-    <Router>
-      <Routes>
+    <Router>     
 
+        <div className='main-body'>
+
+          <Sidebar />
+
+        <Routes>
         <Route path='/' element={ <Library /> } />
         <Route path='/feed' element={ <Feed /> } />
         <Route path='/trending' element={ <Trending /> } />
         <Route path='/player' element={ <Player /> } />
         <Route path='/favorites' element={ <Favorites /> } />
-          
+        </Routes> 
 
-      </Routes>  
+        </div>         
+     
     </Router>
     
   )
