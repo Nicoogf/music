16fda53706f8957079045547dcfac336edbb6558
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-01 13:56:55 -0300

    Se agrega la clase .active a los elementos del sidebar y la animacion Hover

--- src/components/sidebar/sidebarButton.js
+++ src/components/sidebar/sidebarButton.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom' ;
+import { Link, useLocation } from 'react-router-dom' ;
 import './sidebarButton.css'
 import { IconContext } from 'react-icons';
 
 export default function SidebarButton(props) {
+
+    const location = useLocation();
+
+    const isActive = location.pathname === props.to
+
+    const btnClass = isActive ? "btn-body active" : "btn-body"
+
+
   return (
 
     <Link to={props.to}>
 
-        <div className='btn-body'>
+        <div className={btnClass}>
 
             <IconContext.Provider value={{size: "24px", className:"btn-icon"}}>
 
@@ -16,7 +24,7 @@ export default function SidebarButton(props) {
                 <p className='btn-title'> {props.title} </p>
 
             </IconContext.Provider>
-            
+
         </div>
 
     </Link>
