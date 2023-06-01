7a0e64513c00c50abe25b00389b1408347a1c76e
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-01 12:24:44 -0300

    Se empieza a trabajar el sidebar y se agrega react-icons al proyecto

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import './sidebar.css'
+import SidebarButton from './sidebarButton'
 
 export default function Sidebar() {
   return (
-    <div className='sidebar-container'>Sidebar</div>
+    <div className='sidebar-container'>
+
+      <img src ="https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/02/08210136/barijho-14.jpg" 
+           className='profile-img' 
+           alt="profile" /> 
+
+      <div>
+          <SidebarButton title="" to="" icon={}/> 
+          <SidebarButton /> 
+          <SidebarButton /> 
+          <SidebarButton /> 
+          <SidebarButton /> 
+      </div>
+
+      <SidebarButton /> 
+
+    </div>
   )
 }
