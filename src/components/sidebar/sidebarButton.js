258fe6d857a2a0c8fcb59485425a58afc46785cf
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-01 13:44:28 -0300

    Se alinean los botones del menu

--- src/components/sidebar/sidebarButton.js
+++ src/components/sidebar/sidebarButton.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom' ;
 import './sidebarButton.css'
+import { IconContext } from 'react-icons';
 
 export default function SidebarButton(props) {
   return (
+
     <Link to={props.to}>
 
-        <div>{props.icon}</div>
+        <div className='btn-body'>
+
+            <IconContext.Provider value={{size: "24px", className:"btn-icon"}}>
+
+                {props.icon}
+                <p className='btn-title'> {props.title} </p>
+
+            </IconContext.Provider>
+            
+        </div>
 
     </Link>
    
