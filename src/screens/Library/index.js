cd3b0875c92bee888df60cb03fa1f7cf4dd021af
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-02 12:30:57 -0300

    Se crea logica para la visualizacion de playlist del usuario

--- src/screens/Library/index.js
+++ src/screens/Library/index.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React , {useEffect, useState} from 'react'
+import APIKit from "../../spotify"
 
 export default function Library() {
+
+  const[playlists , setPlaylist] = useState(null)
+
+
+  useEffect(()=>{
+    APIKit.get("me/playlists").then(function(response){
+      setPlaylist(response.data.items)
+      console.log(response.data.items);
+    })
+  },[]) 
+
   return (
-    <div className='screen-container'>L</div>
-  )
+    <div className='screen-container'>
+
+      { playlists?.map((playlist) =>(
+        <div> {playlist.name}</div>
+     ))}
+    </div>
+  );
 }
