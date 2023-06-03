4908d32f42057082cb7e349ec91f627ff2e56503
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-03 19:44:49 -0300

    Se agregan estilos al card

--- src/screens/Library/index.js
+++ src/screens/Library/index.js
@@ -1,6 +1,8 @@
-import React , {useEffect, useState} from 'react'
-import APIKit from "../../spotify"
-import "./library.css"
+import React , {useEffect, useState} from 'react' ;
+import APIKit from "../../spotify" ;
+import {IconContext} from "react-icons" ;
+import {AiFillPlayCircle} from "react-icons/ai" ;
+import "./library.css" ;
 
 export default function Library() {
 
@@ -9,23 +11,41 @@ export default function Library() {
 
   useEffect(()=>{
     APIKit.get("me/playlists").then(function(response){
-      setPlaylist(response.data.items)
-      console.log(response.data.items);
+      setPlaylist(response.data.items);
     })
   },[]) 
 
   return (
-    <div className='screen-container'>
 
+    <div className='screen-container'>
       <div className='library-body'> 
       
-        { playlists?.map((playlist) =>(
-          <div className='playlist-card'> {playlist.name}</div>
-      ))}
-      
-      </div>
+      {playlists?.map( (playlist)=>(
 
-   
+        <div className='playlist-card'>
+
+        <img
+              src={playlist.images[0].url}
+              className="playlist-image"
+              alt="Playlist-Art"
+            />
+            <p className="playlist-title">{playlist.name}</p>
+            <p className="playlist-subtitle">{playlist.tracks.total} Songs</p>
+            <div className="playlist-fade">
+              <IconContext.Provider value={{size:"50px",color:"#E99D72"}}>
+                <AiFillPlayCircle />
+              </IconContext.Provider>
+            </div>
+
+        </div>
+      ))}
+
+     
+ 
+
+     
+      
+      </div>   
     </div>
   );
 }
