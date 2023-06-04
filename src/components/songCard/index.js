b0a21f617341c314e19131dc55da658e3fc03ecf
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-04 18:58:07 -0300

    Se trabaja sobre el /Player

---
+++ src/components/songCard/index.js
@@ -0,0 +1,8 @@
+import React from 'react' ;
+import "./songCard.css"
+
+export default function songCard() {
+  return (
+    <div>index</div>
+  )
+}
--- src/screens/player/Index.js
+++ src/screens/player/Index.js
@@ -1,23 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./player.css"
 import {useLocation} from "react-router-dom"
 import apiClient from '../../spotify';
+import SongCard from '../../components/songCard';
+import Queue from '../../components/queue/queue';
 
 export default function Player() {
 
 
   const location = useLocation() ;
+  const [tracks, setTracks] = useState([]) ;
+  const [currentTrack , setCurrentTrack] = useState({}) ;
+  const [currentIndex , setCurrentIndex] = useState(0) ;
+
+
+
   useEffect( ()=>{
     if(location.state){
       apiClient.get("playlists/"+ location.state?.id +"/tracks")
-      .then( res => console.log( (res.data)));
-      
+      .then( res => {
+        setTracks(res.data.items);
+        setCurrentTrack(res.data.items[0].track) });
     }
   },[location.state])
 
   return (
 
-      <div className="screen-container">
+      <div className="screen-container flex">
 
       <div className="left-player-body">
 
@@ -25,6 +34,8 @@ export default function Player() {
 
       <div className="right-player-body">
         
+        <SongCard />
+          <Queue />
       </div>
 
     </div>
