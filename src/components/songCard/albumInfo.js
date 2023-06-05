95ce727863bc42e8dc39041f11637850d498ba55
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-05 09:00:43 -0300

    Diseño de la estructura del  Album-Container

--- src/components/songCard/albumInfo.js
+++ src/components/songCard/albumInfo.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import "./albumInfo.css"
 
-export default function albumInfo() {
+export default function albumInfo({album} ) {
+
+  const artists = [] ;
+
+  album?.artists?.forEach(element => {
+    artists.push(element.name)
+  });
+
+
   return (
-    <div>albumInfo</div>
+
+    <div>
+
+    <div className='albumName-container'>
+
+      <p> {album.name+ "-" + artists?.join(",")} </p>
+
+    </div>
+
+    <div className='album-info'>
+
+      <p> {`${album.name} is an ${album?.album_type} by ${artists?.join(",")} with ${album.total_tracks} tracks`} </p>
+
+    </div>
+
+    <div className='album-release'>
+
+      <p> Release Date : ${album?.release_date}</p>
+
+    </div>
+
+    </div>
   )
 }
