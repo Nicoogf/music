0f348ecbc25b7c9e45f7ec877699869dcc1a6599
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-06 13:59:32 -0300

    Se termina el progress y wave

--- src/components/audioPlayer/index.js
+++ src/components/audioPlayer/index.js
@@ -1,4 +1,4 @@
-import React ,{useState , useRef}from 'react'
+import React ,{useState , useRef, useEffect}from 'react'
 import "./audioPlayer.css"
 import ProgressCicle from "./progresCircle"
 import Controls from './controls';
@@ -21,6 +21,7 @@ export default function audioPlayer(currentTrack,currentIndex, setCurrentIndex ,
 
   const currentPercentage = duration ? (trackProgress / duration) * 100 : 0 ;
 
+  
   const startTimer = () =>{
     clearInterval(intervalRef.current)
 
@@ -32,57 +33,109 @@ export default function audioPlayer(currentTrack,currentIndex, setCurrentIndex ,
         }
       }, [1000]
    )}
+
+   useEffect(()=>{
+    if(isPlaying && audioRef.current){
+      audioRef.current = new Audio (audioSrc)
+      audioRef.current.play();
+      startTimer();
+    }else{
+      clearInterval(intervalRef.current)
+      audioPlayer.current.pause()
+    }
+   },[isPlaying])
+
+   useEffect(()=>{
+    audioRef.current.pause();
+    audioRef.current = new Audio(audioSrc);
+
+   setTrackProgress( audioRef.current.currentTime )
+
+   if(isReady.current){
+     audioRef.current.play();
+     setIsPlayer(true);
+     startTimer();
+   }else{
+     isReady.current = true
+   }
+ },[currentIndex])
+
+
+ useEffect(()=>{
+
+   return()=>{
+     audioRef.current.pause();
+     clearInterval(interval.current)
+   }
+ },[])
+
+ const handleNext = () =>{
+   if( currentIndex < total.length - 1){
+     setCurrentIndex (currentIndex + 1 )
+    } else setCurrentIndex[0]
+   } 
+ 
+   const handlePrev = () =>{
+     if( currentIndex  -1 < 0){
+       setCurrentIndex (total.length -1);
+      } else currentIndex(currentIndex - 1 )
+     } 
+
+   const addZero = (n) =>{
+     return n > 9 ? "" + n :"0" + n
+   }
+
+ const artists = [] ;
+   currentTrack?.album?.artists.forEach(artist => {
+     artists.push(artist.name)
+   });
+ 
+ 
+ return (
+   <div className='player-body flex'>
+
+       <div className='player-left-body'>
+
+           <ProgressCicle 
+           percentage={currentPercentage}
+           isPlaying={true}
+           image={currentTrack?.album?.images[0]?.url}
+           size={300}
+           color="#C96850"
+           />
+
+       </div>
+
+
+       <div className='player-right-body flex'>
+
+       <p className='song-title'> {currentTrack?.name} </p>  
+       <p className='song-artist'> {artists.join(" | ")} </p>
+       <div className="player-right-bottom flex">
+
+         <div className='song-duration flex'>
+           <p className='duration'> 0:{ addZero( Math.round(trackProgress) )} </p>
+           <WaveAnimation isPlaying={isPlaying} />
+           <p className='duration'> 0:30 </p>
+         </div>
+
+         <Controls
+          isPlaying={isPlaying}
+           setIsPlaying={setIsPlaying}
+           handleNext={handleNext}
+           handlePrev={handlePrev}
+           total = {total}
+         />
+
+          
+
+       </div>
+
+       </div>        
+       
+   </div>
+ )
+
   }
 
-
-  const artists = [] ;
-    currentTrack?.album?.artists.forEach(artist => {
-      artists.push(artist.name)
-    });
-
-
-  return (
-    <div className='player-body flex'>
-
-        <div className='player-left-body'>
-
-            <ProgressCicle 
-            percentage={currentPercentage}
-            isPlaying={true}
-            image={currentTrack?.album?.images[0]?.url}
-            size={300}
-            color="#C96850"
-            />
-
-        </div>
-
-
-        <div className='player-right-body flex'>
-
-        <p className='song-title'> {currentTrack?.name} </p>  
-        <p className='song-artist'> {artists.join(" | ")} </p>
-        <div className="player-right-bottom flex">
-
-          <div className='song-duration flex'>
-            <p className='duration'> 0:01 </p>
-            <WaveAnimation isPlaying={isPlaying} />
-            <p className='duration'> 0:30 </p>
-          </div>
-
-          <Controls
-           isPlaying={isPlaying}
-            setIsPlaying={setIsPlaying}
-            handleNext={handleNext}
-            handlePrev={handlePrev}
-            total = {total}
-          />
-
-           
-
-        </div>
-
-        </div>        
-        
-    </div>
-  )
-}
+  
