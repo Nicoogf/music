35cd1cab7ec9592ac3bd23f3a5d32af2d49c940e
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-07 12:21:55 -0300

    Fix Player

--- src/screens/player/Index.js
+++ src/screens/player/Index.js
@@ -1,59 +1,57 @@
-import React, { useEffect, useState } from 'react' ;
-import "./player.css" ;
-import {useLocation} from "react-router-dom" ;
-import apiClient from '../../spotify';
-import SongCard from '../../components/songCard' ;
-import Queue from '../../components/queue/queue' ;
-import AudioPlayer from '../../components/audioPlayer' ;
-import Widgets from '../../components/widgets';
+import React, { useEffect, useState } from "react";
+import "./player.css";
+import { useLocation } from "react-router-dom";
+import apiClient from "../../spotify";
+import SongCard from "../../components/songCard";
+import Queue from "../../components/queue";
+import AudioPLayer from "../../components/audioPlayer";
+import Widgets from "../../components/widgets";
 
 export default function Player() {
+  const location = useLocation();
+  const [tracks, setTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-
-  const location = useLocation() ;
-  const [tracks, setTracks] = useState([]) ;
-  const [currentTrack , setCurrentTrack] = useState({}) ;
-  const [currentIndex , setCurrentIndex] = useState(0) ;
-
-
-
-  useEffect( ()=>{
-    if(location.state){
-      apiClient.get("playlists/"+ location.state?.id +"/tracks")
-      .then( res => {
-        setTracks(res.data.items);
-        setCurrentTrack(res.data.items[0].track) });
+  useEffect(() => {
+    if (location.state) {
+      apiClient
+        .get("playlists/" + location.state?.id + "/tracks")
+        .then((res) => {
+          setTracks(res.data.items);
+          setCurrentTrack(res.data?.items[0]?.track);
+        });
     }
-  },[location.state])
+  }, [location.state]);
 
-  useEffect(()=>{
-    setCurrentTrack(tracks[currentIndex].track)
-  },[currentIndex,tracks]) ;
+  useEffect(() => {
+    setCurrentTrack(tracks[currentIndex]?.track);
+  }, [currentIndex, tracks]);
 
   return (
-
-      <div className="screen-container flex">
+    <div className="screen-container flex">
 
       <div className="left-player-body">
 
-        <AudioPlayer 
-          currentTrack={currentTrack} 
+        <AudioPLayer
+          currentTrack={currentTrack}
           total={tracks}
-          isPlaying={true} 
-          currentIndex={currentIndex} 
-          setCurrentIndex={setCurrentIndex}/>
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+        />
 
-          <Widgets
-          artistID={currentTrack?.album?.artists[0]?.id}/>
+        <Widgets artistID={currentTrack?.album?.artists[0]?.id} />
 
       </div>
 
       <div className="right-player-body">
-        
-        <SongCard album={currentTrack.album}/>
-          <Queue tracks={tracks} setCurrentIndex={setCurrentIndex}/>
-      </div>
 
+        <SongCard album={currentTrack?.album} />
+
+        <Queue tracks={tracks} setCurrentIndex={setCurrentIndex} />
+
+      </div>
+      
     </div>
-  )
+  );
 }
