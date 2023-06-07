d865b36b8a8b3cb4e1f6b7c354df098d3d1ba13a
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-07 12:20:42 -0300

    Fix Library

--- src/screens/Library/index.js
+++ src/screens/Library/index.js
@@ -1,60 +1,64 @@
-import React , {useEffect, useState} from 'react' ;
-import APIKit from "../../spotify" ;
-import {IconContext} from "react-icons" ;
-import {AiFillPlayCircle} from "react-icons/ai" ;
-import "./library.css" ;
-import { useNavigate } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import APIKit from "../../spotify";
+import { IconContext } from "react-icons";
+import { AiFillPlayCircle } from "react-icons/ai";
+import "./library.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Library() {
+  const [playlists, setPlaylists] = useState(null);
 
-  const[playlists , setPlaylist] = useState(null)
+  useEffect(() => {
+    APIKit.get("me/playlists").then(function (response) {
+      setPlaylists(response.data.items);
+    });
+  }, []);
 
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-    APIKit.get("me/playlists").then(function(response){
-      setPlaylist(response.data.items);
-    })
-  },[]) 
-
-  const navidate =useNavigate()
-  
-
-  const playPlayList = (id) =>{
-    navidate("/player" , { state: {id : id} } )
-  }
-
+  const playPlaylist = (id) => {
+    navigate("/player", { state: { id: id } });
+  };
 
   return (
+    <div className="screen-container">
 
-    <div className='screen-container'>
-      <div className='library-body'> 
-      
-      {playlists?.map( (playlist)=>(
+      <div className="library-body">
 
-        <div className='playlist-card' key={playlist.id} onClick={()=> playPlayList(playlist.id)}>
+        {playlists?.map((playlist) => (
 
-        <img
+          <div
+            className="playlist-card"
+            key={playlist.id}
+            onClick={() => playPlaylist(playlist.id)}
+          >
+
+            <img
               src={playlist.images[0].url}
               className="playlist-image"
               alt="Playlist-Art"
             />
+
             <p className="playlist-title">{playlist.name}</p>
+           
             <p className="playlist-subtitle">{playlist.tracks.total} Songs</p>
+           
             <div className="playlist-fade">
-              <IconContext.Provider value={{size:"50px",color:"#E99D72"}}>
+
+             
+              <IconContext.Provider value={{ size: "50px", color: "#E99D72" }}>
+               
                 <AiFillPlayCircle />
+             
               </IconContext.Provider>
+           
             </div>
-
-        </div>
-      ))}
-
+          </div>
+       
+       ))}
      
- 
-
-     
-      
-      </div>   
+      </div>
+    
     </div>
   );
 }
